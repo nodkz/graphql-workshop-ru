14230d131c27c09d75c962864478e7518c570ff3
nodkz <pavel.chertorogov@gmail.com>
2019-01-26 13:02:11 +0300

    implement simple server with graphql-js

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,36 @@
-console.log('123!!!!');
+import { ApolloServer } from 'apollo-server';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLNonNull } from 'graphql';
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: () => ({
+      hello: {
+        args: {
+          name: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: 'Please provide your name',
+          },
+        },
+        resolve: (source, args, context, info) => {
+          return `HELLO, ${args.name}`;
+        },
+        type: GraphQLString,
+      },
+    }),
+  }),
+});
+
+const server = new ApolloServer({
+  schema,
+  playground: true,
+});
+
+server.listen(
+  {
+    port: 4000,
+    endpoint: '/graphql',
+    playground: '/playground',
+  },
+  () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
